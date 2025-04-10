33005a8b72efe5b7511a6c5ae913e6bf31929193
Rozimurodov <rozimurodov@Rozimurodovs-MacBook-Pro.local>
2025-04-11 00:50:32 +0500

    I create web site login register

---
+++ js/login.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    const user = false
+    if (user) {
+        window.location.href = "home.html";
+    }
+})
---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    const user = null
+    if (user === null) {
+        window.location.href = "login.html"
+    }
+})
